4b83fc8b3b71644d65cde75577ac635efd80f7d0
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-09-29 20:05:27 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Profile from './Components/SocialProfile/Profile';
+import Statistics from './Components/Statistics/Statistics.jsx';
+import user from './Components/Data/user.json';
+import friends from './Components/Data/friends.json';
+import statisticalData from './Components/Data/statistical-data.json';
+import transactions from './Components/Data/transactions.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+    </>
+  );
+}
